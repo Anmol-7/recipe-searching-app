2f642880a18577ccae17c7f76d337ee385fafe6f
Anmol-7 <jainanmol5528@gmail.com>
2020-06-20 05:22:42 +0530

    chnages

---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('Imported modiule');
+export default 23;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path= require('path');
 module.exports={
-    entry: './src/js/index.js',
-    output:{
-        path:'',
-        filename: 'bundle.js'
-    }
+entry: './src/js/index.js',
+output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
+},
+ node: 'development'
 };
